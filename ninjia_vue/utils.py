3fd03e4b36c512471d4be9fafc8bd5b9ba381922
peter <huozhiniao@sina.com>
2024-04-17 15:30:30 +0800

    修改token验证函数

--- ninjia_vue/utils.py
+++ ninjia_vue/utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
+from django.http import HttpRequest
 from jose import jwt
+from ninja.security import HttpBearer
 
 
 def generate_token(username: str, expire_minutes: int=1):
@@ -10,9 +12,14 @@ def generate_token(username: str, expire_minutes: int=1):
     :return 编码后jwt字符串
     '''
     expire = datetime.now() + timedelta(minutes=expire_minutes)
-    to_encode = {'sub': username, 'expire': expire}
+    to_encode = {'sub': username, 'exp': expire}
     encoded_jwt = jwt.encode(to_encode, 'ninja_vue', algorithm='HS256')
 
     return encoded_jwt
 
+
+class TokenAuth(HttpBearer):
+    def authenticate(self, request: HttpRequest, token: str) -> jwt.Any | None:
+        payload = jwt.decode(token, 'ninja_vue', algorithm='HS256')
+        username: str = payload.get('sub')
 
